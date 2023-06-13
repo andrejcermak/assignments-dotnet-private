78c79e21d2d6ba4887410762eebc9cb1d26cc4da
Andrej Cermak <andrej.cermak@vacuumlabs.com>
2023-06-13 14:52:48 +0200

    Password hasher integrated to the identity service

--- sln/IdentityService/IdentityService.cs
+++ sln/IdentityService/IdentityService.cs
@@ -2,29 +2,28 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 
 using Datamole.InterviewAssignments.IdentityService.Models;
 
 using System.Text.Json;
+
+using Datamole.InterviewAssignments.IdentityService.Helpers;
 
 namespace Datamole.InterviewAssignments.IdentityService
 {
     public class IdentityService : IIdentityService
     {
 
-        private Dictionary<string, UserData> _database = new();
+        internal Dictionary<string, UserData> Database { get; set; }
+        internal PasswordHasher PasswordHasher { get; set; }
 
-        internal IdentityService(Dictionary<string, UserData> database)
-        {
-            _database = database;
-        }
-        
         public RegistrationResult Register(string userName, string password,
             IDictionary<string, string>? properties = null)
         {
-            if (!_database.ContainsKey(userName.ToLower()))
+            if (!Database.ContainsKey(userName.ToLower()))
             {
-                _database.Add(userName.ToLower(), new UserData(userName, password, properties?? new Dictionary<string, string>()));
+                Database.Add(userName.ToLower(), new UserData(userName, PasswordHasher.HashPassword(password), properties?? new Dictionary<string, string>()));
                 return RegistrationResult.Successful();
             }
 
@@ -33,13 +32,13 @@ namespace Datamole.InterviewAssignments.IdentityService
 
         public AuthenticationResult Authenticate(string userName, string password)
         {
-            var userData = _database.GetValueOrDefault(userName.ToLower());
+            var userData = Database.GetValueOrDefault(userName.ToLower());
             if (userData is null)
             {
                 return AuthenticationResult.Failed(AuthenticationError.UserNotFound);
             }
 
-            if (userData.Password != password)
+            if (!PasswordHasher.VerifyPassword(password, userData.Password.HashedPassword, userData.Password.Salt))
             {
                 return AuthenticationResult.Failed(AuthenticationError.InvalidPassword);
             }
@@ -49,7 +48,7 @@ namespace Datamole.InterviewAssignments.IdentityService
 
         public void SaveToJson(string pathToJsonFile, bool overwrite = false)
         {
-            var result = JsonSerializer.Serialize(_database);
+            var result = JsonSerializer.Serialize(Database);
             Console.WriteLine(pathToJsonFile);
             if (!overwrite && File.Exists(pathToJsonFile))
             {
@@ -63,9 +62,9 @@ namespace Datamole.InterviewAssignments.IdentityService
         {
             public string Name { get; set; }
             public IDictionary<string, string> Properties { get; set; }
-            public string Password { get; set; }
+            public PasswordObject Password { get; set; }
 
-            public UserData(string name, string password, IDictionary<string, string>? properties)
+            public UserData(string name, PasswordObject password, IDictionary<string, string>? properties)
             {
                 Name = name;
                 Password = password;
--- sln/IdentityService/IdentityServiceFactory.cs
+++ sln/IdentityService/IdentityServiceFactory.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 
+using Datamole.InterviewAssignments.IdentityService.Helpers;
+
 namespace Datamole.InterviewAssignments.IdentityService
 {
     public static class IdentityServiceFactory
@@ -11,9 +13,9 @@ namespace Datamole.InterviewAssignments.IdentityService
         public static IIdentityService CreateFromJson(string pathToJsonFile)
         {
             var fileContents = File.ReadAllText(pathToJsonFile);
-            Console.WriteLine(fileContents);
             var database = JsonSerializer.Deserialize<Dictionary<string, IdentityService.UserData>>(fileContents);
-            return new IdentityService(database);
+            var service = new IdentityService { PasswordHasher = new PasswordHasher(), Database = database };
+            return service;
         }
 
         public static IIdentityService CreateFromMemory(IEnumerable<string> users, IEnumerable<string> passwords)
@@ -23,16 +25,16 @@ namespace Datamole.InterviewAssignments.IdentityService
                 throw new Exception("Invalid input");
             }
 
+            var service = new IdentityService { PasswordHasher = new PasswordHasher() };
             var database = new Dictionary<string, IdentityService.UserData>();
             foreach (var (userName, password) in users.Zip(passwords))
             {
                 database.Add(userName.ToLower(),
-                    new IdentityService.UserData(userName, password, new Dictionary<string, string>()));
-                Console.WriteLine(
-                    JsonSerializer.Serialize(new IdentityService.UserData(userName, password,
-                        new Dictionary<string, string>())));
+                    new IdentityService.UserData(userName, service.PasswordHasher.HashPassword(password), new Dictionary<string, string>()));
             }
-            return new IdentityService(database);
+
+            service.Database = database;
+            return service;
         }
     }
 }
