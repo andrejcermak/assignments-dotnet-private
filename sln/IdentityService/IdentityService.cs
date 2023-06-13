cb40533ae10d0ab69fbba0d7aa8d1e0fb54b3310
Andrej Cermak <andrej.cermak@vacuumlabs.com>
2023-06-13 10:59:30 +0200

    Modified db dictionary to contain object instead of tuple

--- sln/IdentityService/IdentityService.cs
+++ sln/IdentityService/IdentityService.cs
@@ -1,35 +1,51 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 using Datamole.InterviewAssignments.IdentityService.Models;
 
+using System.Text.Json;
+
 namespace Datamole.InterviewAssignments.IdentityService
 {
-    public class IdentityService: IIdentityService
+    public class IdentityService : IIdentityService
     {
-        private List<string> _users;
-        private List<string> _passwords;
-        
-        
-        public IdentityService(IEnumerable<string> users, IEnumerable<string> passwords)
-        {
-            _users = users.ToList();
-            _passwords = passwords.ToList();
-        }
 
+        private Dictionary<string, UserData> _database = new();
+
+        internal IdentityService(Dictionary<string, UserData> database)
+        {
+            _database = database;
+        }
+        
         public RegistrationResult Register(string userName, string password,
             IDictionary<string, string>? properties = null)
         {
-            if (_users.FirstOrDefault(user => user.ToLower().Equals(userName.ToLower())) is null)
+            if (!_database.ContainsKey(userName.ToLower()))
             {
-                _users.Add(userName);
-                _passwords.Add(password);
+                _database.Add(userName.ToLower(), new UserData(userName, password, properties?? new Dictionary<string, string>()));
                 return RegistrationResult.Successful();
             }
+
             return RegistrationResult.Failed(RegistrationError.UserAlreadyExists);
         }
 
-        public AuthenticationResult Authenticate(string userName, string password) => throw new System.NotImplementedException();
+        public AuthenticationResult Authenticate(string userName, string password)
+        {
+            var userData = _database.GetValueOrDefault(userName.ToLower());
+            if (userData is null)
+            {
+                return AuthenticationResult.Failed(AuthenticationError.UserNotFound);
+            }
+
+            if (userData.Password != password)
+            {
+                return AuthenticationResult.Failed(AuthenticationError.InvalidPassword);
+            }
+
+            return AuthenticationResult.Successful(userData.Name, userData.Properties);
+        }
 
         public void SaveToJson(string pathToJsonFile, bool overwrite = false) => throw new System.NotImplementedException();
     }
