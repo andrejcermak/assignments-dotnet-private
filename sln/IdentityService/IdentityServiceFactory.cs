181f71107b4b9e044c3512dc728e209a01f51291
Andrej Cermak <andrej.cermak@vacuumlabs.com>
2023-06-13 14:04:11 +0200

    Moved the service initialization to factory

--- sln/IdentityService/IdentityServiceFactory.cs
+++ sln/IdentityService/IdentityServiceFactory.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 
 namespace Datamole.InterviewAssignments.IdentityService
 {
@@ -8,9 +10,10 @@ namespace Datamole.InterviewAssignments.IdentityService
     {
         public static IIdentityService CreateFromJson(string pathToJsonFile)
         {
-            // TODO: Implement
-
-            throw new NotImplementedException();
+            var fileContents = File.ReadAllText(pathToJsonFile);
+            Console.WriteLine(fileContents);
+            var database = JsonSerializer.Deserialize<Dictionary<string, IdentityService.UserData>>(fileContents);
+            return new IdentityService(database);
         }
 
         public static IIdentityService CreateFromMemory(IEnumerable<string> users, IEnumerable<string> passwords)
@@ -18,8 +21,18 @@ namespace Datamole.InterviewAssignments.IdentityService
             if (users.Count() != passwords.Count())
             {
                 throw new Exception("Invalid input");
-            }    
-            return new IdentityService(users, passwords);
+            }
+
+            var database = new Dictionary<string, IdentityService.UserData>();
+            foreach (var (userName, password) in users.Zip(passwords))
+            {
+                database.Add(userName.ToLower(),
+                    new IdentityService.UserData(userName, password, new Dictionary<string, string>()));
+                Console.WriteLine(
+                    JsonSerializer.Serialize(new IdentityService.UserData(userName, password,
+                        new Dictionary<string, string>())));
+            }
+            return new IdentityService(database);
         }
     }
 }
